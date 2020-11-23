63f54c6813c72aaae155cf4301eeea6fb04c3035
rey-Adriano <74863809+rey-Adriano@users.noreply.github.com>
2020-11-23 10:07:13 -0500

    Add files via upload

---
+++ preg4.php
@@ -0,0 +1,18 @@
+<?php
+
+class libro{
+    public $nombrelibro; 
+    public $ISBN;
+ public $nombreAutor;
+ public $nombreEditor;
+
+
+ function __construct(string $nombrelibro, int $ISBN, string $nombreAutor, string $nombreEditor)
+ {
+     
+ }
+    
+}
+
+
+?>
